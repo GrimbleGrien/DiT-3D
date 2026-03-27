99296ed2c9897c1adb9050c2537a7339080ae92c
GrimbleGrien <iwillachievewhatiwant@gmail.com>
2026-03-27 15:52:30 +0530

    update scipt

--- scripts/train_S4_chair_mae.sh
+++ scripts/train_S4_chair_mae.sh
@@ -1,16 +1,17 @@
-ython train.py --gpu 0 \
+python train.py --gpu 0 \
     --dataroot ../../PSF/data/ShapeNetCore.v2.PC15k/ \
     --category chair \
-    --experiment_name 'mae1000' \
+    --experiment_name 'mae500' \
     --model_type 'DiT-S/4' \
     --bs 16 \
     --voxel_size 32 \
     --lr 1e-4 \
     --use_tb \
-    --niter 1000 \
+    --niter 500 \
     --saveIter 1 \
     --use_mae \
     --mae_config_path 'configs/pretrainMAE.yaml' \
     --mae_points 1024 \
     --mae_mask_ratio 0.6 \
     --vizIter 100 \
+    # --model 'checkpoints/mae1000/latest.pth'
